4b47ae49cda2d7c8529e51e6a730288e89aed978
Shivam-99 <37024449+Shivam-99@users.noreply.github.com>
2020-05-12 02:04:09 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using Microsoft.Azure.Devices.Client;
+
+namespace IOT_Hub_Receiver
+{
+    class Program
+    {
+        static DeviceClient deviceClient;
+        static string connectionstring = "HostName=DemoIoTHubApp.azure-devices.net;DeviceId=IotDeviceDemo;SharedAccessKey=f103jW1XArqRi4ZILkQ0/Zx7mqXBxbsmBBU/yopRMaY=";
+        static void Main(string[] args)
+        {
+            deviceClient = DeviceClient.CreateFromConnectionString(connectionstring);
+            ReceiveC2dAsync();
+            Console.ReadLine();
+        }
+        private static async void ReceiveC2dAsync()
+        {
+            Console.WriteLine("Receiving message from Cloud to Device");
+            while (true)
+            {
+                Message recievedmessage = await deviceClient.ReceiveAsync();
+
+                Console.WriteLine("Received message: {0}",
+                    Encoding.ASCII.GetString(recievedmessage.GetBytes()));
+
+                await deviceClient.CompleteAsync(recievedmessage);
+            }
+        }
+    }
+}
